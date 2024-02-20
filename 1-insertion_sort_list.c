610d53eb3c32cfa15b15ba033ace7dc69064eec2
Ziad-Abdel-Mabood <ziadabdlemabood@gmail.com>
2024-02-20 04:15:33 +0200

    proj 0x1B, task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,67 @@
+#include "sort.h"
+
+void swap_dbl_list(listint_t *node);
+
+
+/**
+ * insertion_sort_list -	sorts a doubly linked list of ints in an ascending
+ *				order using the insertion sort algorithm.
+ * @list:			list to be sorted.
+ * Return:			nothing.
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *ptr1 = (*list)->next;
+	listint_t *ptr2;
+
+	/* iterate over the list */
+	while (ptr1)
+	{
+		ptr2 = ptr1;
+		/**
+		 * start from second node, if its data is less than the previous
+		 * node; then swap(send it backwards) and check again.
+		 */
+		while (ptr2->prev && ptr2->prev->n > ptr2->n)
+		{
+			swap_dbl_list(ptr2);
+
+			while ((*list)->prev)
+				*list = (*list)->prev;
+			print_list(*list);
+		}
+		ptr1 = ptr1->next;
+	}
+}
+
+/**
+ * swap_dbl_list -	swaps a node in a doubly linked list with its previous node.
+ * @node:		node to be swapped.
+ * Return:		nothing.
+ */
+void swap_dbl_list(listint_t *node)
+{
+	listint_t *temp;
+
+	temp = node->prev;
+
+	if (temp->prev)
+	{
+		node->prev = temp->prev;
+		node->prev->next = node;
+	}
+	else	/* checking if we reached the start of the list */
+		node->prev = NULL;
+
+	temp->prev = node;
+
+	if (node->next)
+	{
+		temp->next = node->next;
+		node->next->prev = temp;
+	}
+	else	/* we reached the end of the list */
+		temp->next = NULL;
+
+	node->next = temp;
+}
